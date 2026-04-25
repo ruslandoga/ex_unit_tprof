e1d585c1288d83f42cf83cd0571772594d307645
ruslandoga <ruslandoga+gh@icloud.com>
2026-04-25 14:21:40 +0300

    init

---
+++ lib/ex_unit_t_prof.ex
@@ -0,0 +1,18 @@
+defmodule ExUnitTProf do
+  @moduledoc """
+  Documentation for `ExUnitTProf`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExUnitTProf.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_unit_t_prof_test.exs
@@ -0,0 +1,8 @@
+defmodule ExUnitTProfTest do
+  use ExUnit.Case
+  doctest ExUnitTProf
+
+  test "greets the world" do
+    assert ExUnitTProf.hello() == :world
+  end
+end
